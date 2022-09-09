30131d6b37ca6609040a63a9042c3f1d8d311680
shankar wangwad <shankarwangwad2018@gmail.com>
2022-09-09 20:18:41 +0530

    changed profile.js

--- src/views/examples/Profile.js
+++ src/views/examples/Profile.js
@@ -30,7 +30,7 @@ const Profile = () => {
                       <img
                         alt="..."
                         className="rounded-circle"
-                        src={require("../../assets/img/theme/team-4-800x800.jpg")}
+                        src={require("../../assets/img/theme/profile.jpg")}
                       />
                     </a>
                   </div>
@@ -38,15 +38,7 @@ const Profile = () => {
               </Row>
               <CardHeader className="text-center border-0 pt-8 pt-md-4 pb-0 pb-md-4">
                 <div className="d-flex justify-content-between">
-                  <Button
-                    className="mr-4"
-                    color="info"
-                    href="#pablo"
-                    onClick={(e) => e.preventDefault()}
-                    size="sm"
-                  >
-                    Connect
-                  </Button>
+                  
                   <Button
                     className="float-right"
                     color="default"
@@ -63,43 +55,39 @@ const Profile = () => {
                   <div className="col">
                     <div className="card-profile-stats d-flex justify-content-center mt-md-5">
                       <div>
-                        <span className="heading">22</span>
-                        <span className="description">Friends</span>
+                        <span className="heading">52</span>
+                        <span className="description">Attended</span>
                       </div>
                       <div>
                         <span className="heading">10</span>
-                        <span className="description">Photos</span>
+                        <span className="description">Absent</span>
                       </div>
                       <div>
-                        <span className="heading">89</span>
-                        <span className="description">Comments</span>
+                        <span className="heading">416</span>
+                        <span className="description">Room</span>
                       </div>
                     </div>
                   </div>
                 </Row>
                 <div className="text-center">
                   <h3>
-                    Jessica Jones
-                    <span className="font-weight-light">, 27</span>
+                    Shankar Wangwad
+                    {/* <span className="font-weight-light">, Room : 416</span> */}
                   </h3>
                   <div className="h5 font-weight-300">
                     <i className="ni location_pin mr-2" />
-                    Bucharest, Romania
+                    Latur, Maharashtra
                   </div>
-                  <div className="h5 mt-4">
+                 {/*  <div className="h5 mt-4">
                     <i className="ni business_briefcase-24 mr-2" />
                     Solution Manager - Creative Tim Officer
-                  </div>
+                  </div> */}
                   <div>
                     <i className="ni education_hat mr-2" />
-                    University of Computer Science
+                    LIT2020046
                   </div>
                   <hr className="my-4" />
-                  <p>
-                    Ryan — the name taken by Melbourne-raised, Brooklyn-based
-                    Nick Murphy — writes, performs and records all of his own
-                    music.
-                  </p>
+                 
                   <a href="#pablo" onClick={(e) => e.preventDefault()}>
                     Show more
                   </a>
